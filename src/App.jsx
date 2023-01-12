5ce286189db1792ec3c8709008400b1713a7fc0f
paige-clark <paigeclarkillustration@gmail.com>
2023-01-12 09:08:14 -0500

    Add test slime lottie using lottie-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import { saveData } from './assets/saveData'
+import { useState } from "react";
+import Lottie from "lottie-react";
+import "./App.css";
+import { saveData } from "./assets/saveData";
+import slime from "./assets/slime-test.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // const options = {
+  //   animationData: slime,
+  //   loop: true,
+  // };
+  // const { view } = useLottie(options);
 
   return (
     <div className="App">
       <h1>Weather Wizard</h1>
       <div className="card">
+      <div>
+        <Lottie className="logo" animationData={slime} loop={true} />
+      </div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -17,7 +27,7 @@ function App() {
         </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
