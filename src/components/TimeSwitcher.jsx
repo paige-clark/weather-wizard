67d9082106f915fb127b3233b128093abcb24605
paige-clark <paigeclarkillustration@gmail.com>
2023-01-23 10:37:25 -0500

    Start initial TimeSwitcher format

--- src/components/TimeSwitcher.jsx
+++ src/components/TimeSwitcher.jsx
@@ -12,47 +12,56 @@ function TimeSwitcher({ date, currentDate, setCurrentDate }) {
   return (
     <div className="TimeSwitcher">
       {/* decrement the time of day */}
-      <button
-        onClick={() => {
-          if (currentDate.timeOfDay === "morning") {
-            dateFormatter(false, true);
-          }
-          setCurrentDate((currentDate) =>
-            timeBackward(currentDate.timeOfDay, currentDate.currentDay)
-          );
-        }}
-      >
-        &lt;
-      </button>
-      {currentDate.timeOfDay}
-      {/* increment the time of day */}
-      <button
-        onClick={() => {
-          if (currentDate.timeOfDay === "overnight") {
-            // issue: dayGenerator should only be called if the next day doesn't exist
+      <div className="switcher-mini-calendar">
+        <div className="switcher-mini-calendar-month">{saveData.currentMonth.monthName}</div>
+        {saveData.dayInMonth}
+      </div>
+      <div>
+        <div>{date.currentDayOfWeek}</div>
+        <div>
+          <button
+            onClick={() => {
+              if (currentDate.timeOfDay === "morning") {
+                dateFormatter(false, true);
+              }
+              setCurrentDate((currentDate) =>
+                timeBackward(currentDate.timeOfDay, currentDate.currentDay)
+              );
+            }}
+          >
+            &lt;
+          </button>
+          {currentDate.timeOfDay}
+          {/* increment the time of day */}
+          <button
+            onClick={() => {
+              if (currentDate.timeOfDay === "overnight") {
+                // issue: dayGenerator should only be called if the next day doesn't exist
 
-            // if days at the index of this day + 1 does not exist call the generator
-            if (!days[saveData.currentDay + 1]) {
-              dayGenerator(currentDate.currentDay + 1);
-            }
+                // if days at the index of this day + 1 does not exist call the generator
+                if (!days[saveData.currentDay + 1]) {
+                  dayGenerator(currentDate.currentDay + 1);
+                }
 
-            dateFormatter(true, false);
+                dateFormatter(true, false);
 
-            // then set the day forward
-            setCurrentDate((currentDate) =>
-              timeForward(currentDate.timeOfDay, currentDate.currentDay)
-            );
-          } else {
-            setCurrentDate((currentDate) =>
-              timeForward(currentDate.timeOfDay, currentDate.currentDay)
-            );
-          }
-        }}
-      >
-        &gt;
-      </button>
-      The date is {date.currentDayOfWeek} day {date.currentDay + 1}. The month
-      is {date.currentMonth.monthName}. It is currently {date.timeOfDay}.
+                // then set the day forward
+                setCurrentDate((currentDate) =>
+                  timeForward(currentDate.timeOfDay, currentDate.currentDay)
+                );
+              } else {
+                setCurrentDate((currentDate) =>
+                  timeForward(currentDate.timeOfDay, currentDate.currentDay)
+                );
+              }
+            }}
+          >
+            &gt;
+          </button>
+        </div>
+      </div>
+      {/* The date is {date.currentDayOfWeek} day {date.currentDay + 1}. The month
+      is {date.currentMonth.monthName}. It is currently {date.timeOfDay}. */}
     </div>
   );
 }
